cfc7d70fc1680dbbf5304622ecca0447743a4fee
Luke Shea <shealuke@yahoo.com>
2023-03-01 12:20:47 -0600

    Fixed the \n issue, program works pretty good now

--- c_programming_language/remove_comments/remove_comments.c
+++ c_programming_language/remove_comments/remove_comments.c
@@ -2,7 +2,6 @@
 #include <stdbool.h>
 #define MAXLINE 1000
 
-// Comment 1
 void getLine(char line[], int maxline);
 void removeComments(char line[]);
 
@@ -30,7 +29,6 @@ int main(int argc, char *argv[]) {
     // Close all files to prevent memory leaks
     fclose(intput_file);
     fclose(output_file);
-    // Comment 2
     
     return 0;
 }
@@ -40,19 +38,14 @@ void removeComments(char line[]) {
     bool comment = false;
     for (int i = 0; i < MAXLINE - 1 && line[i] != '\0'; i++) {
         if (line[i] == '\n' && i > 0) {
-            line[i - 1] = '\n';
+            // fall through
         } else if (comment && line[i] != '\n') {
             line[i] = '\0';
         } else if (line[i] == '/' && i > 0 && line[i - 1] == '/') {
-            line[i - 1] = '\0';
+            line[i - 1] = '\n';
             line[i] = '\0';
             comment = true;
         }
 
     }
 }
-
-// Comment 6
-
-
-
